e9141c003e2a638250a3bcaad41f1712865c7170
chaijewon <vcandjava@nate.com>
2025-12-08 10:30:14 +0900

    1111

--- SpringBootThymeLeafProject_1/src/main/java/com/sist/web/SpringBootThymeLeafProject1Application.java
+++ SpringBootThymeLeafProject_1/src/main/java/com/sist/web/SpringBootThymeLeafProject1Application.java
@@ -4,8 +4,6 @@ package com.sist.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class SpringBootThymeLeafProject1Application {
 
