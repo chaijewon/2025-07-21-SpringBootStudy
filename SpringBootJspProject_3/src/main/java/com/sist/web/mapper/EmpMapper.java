b3afd5a77a96606b431e44d107a56053a774254d
chaijewon <vcandjava@nate.com>
2025-12-22 10:45:36 +0900

    1111

---
+++ SpringBootJspProject_3/src/main/java/com/sist/web/mapper/EmpMapper.java
@@ -0,0 +1,16 @@
+package com.sist.web.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import com.sist.web.vo.*;
+@Mapper
+@Repository
+public interface EmpMapper {
+  @Select("SELECT empno,ename,job,sal,TO_CHAR(hiredate,'YYYY-MM-DD') as dbday "
+		 +"FROM emp ORDER BY empno ASC")
+  public List<EmpVO> empListData();
+  // JOIN / SubQuery / 동적 쿼리 이용 => XML
+}
