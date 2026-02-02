07db9150f00523a3b56ecd0f0c7f18ab257d5128
chaijewon <vcandjava@nate.com>
2026-02-02 16:49:04 +0900

    1111

--- SpringReactProject_1/src/main/java/com/sist/web/controller/RecipeRestController.java
+++ SpringReactProject_1/src/main/java/com/sist/web/controller/RecipeRestController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import java.util.*;
 import com.sist.web.entity.*;
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class RecipeRestController {
    private final RecipeService rService;
    
