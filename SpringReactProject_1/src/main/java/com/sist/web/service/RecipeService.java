d68d1b1899773074dfb31f0097980a6576179cef
chaijewon <vcandjava@nate.com>
2026-02-02 16:18:39 +0900

    1111

---
+++ SpringReactProject_1/src/main/java/com/sist/web/service/RecipeService.java
@@ -0,0 +1,14 @@
+package com.sist.web.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.sist.web.entity.RecipeEntity;
+import com.sist.web.vo.RecipeListVO;
+
+public interface RecipeService {
+	public Page<RecipeEntity> findAll(Pageable pageable);
+	public int recipeTotalPage();
+}
